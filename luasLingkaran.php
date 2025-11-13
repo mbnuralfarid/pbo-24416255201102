4317f3182d2eae6d4437f47b7e454cbcb4d31980
mbnuralfarid <mbnuralfarid@gmail.com>
2025-11-13 10:25:41 +0700

    Add files via upload

--- luasLingkaran.php
+++ luasLingkaran.php
@@ -1,27 +1,13 @@
 <?php
 
-namespace App\Math;
-
 class LuasLingkaran {
     public const phi = 3.14;
-    protected int $jari;
-
-    public function __construct($isiJari = 1) {
-        $this->jari = $isiJari;
-    }
-
-    public function tampil($nama = 'ban') {
-        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
-        echo "Luas Lingkaran {$nama} dengan jari jari $this->jari adalah {$rumus}";
-    }
-
-    public static function testing() {
-        echo "<br>";
-        echo "ini dari static";
-    }
-
-    public function __destruct() {
-        echo "<br>";
-        echo "udh ah cape";
-    }
+    public int $jari;
 }
+
+$Lingkaran = new LuasLingkaran();
+$Lingkaran->jari = 10;
+
+$rumus = LuasLingkaran::phi * ($Lingkaran->jari * $Lingkaran->jari);
+
+echo "Luas Lingkaran dengan jari jari $Lingkaran->jari adalah $rumus";
